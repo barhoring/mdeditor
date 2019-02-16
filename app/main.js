28571cfa960712194ed962b9717cdb7c5b4175e3
Bar Horing <bar.apple@gmail.com>
2019-02-17 00:54:55 +0200

    open file buttons opens a dialog

--- app/main.js
+++ app/main.js
@@ -1,17 +1,36 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require("electron");
+const fs = require("fs");
 
 let mainWindow = null;
 
-app.on('ready', () => {
+const getFileFromUserSelection = (exports.getFileFromUserSelection = () => {
+  const files = dialog.showOpenDialog({
+    properties: ["openFile"],
+    filters: [
+      { name: "Text Files", extensions: ["txt", "text"] },
+      { name: "Markdown Files", extensions: ["md", "markdown"] }
+    ]
+  });
+
+  if (!files) return;
+
+  const file = files[0];
+  const content = fs.readFileSync(file).toString();
+
+  console.log(content);
+});
+
+app.on("ready", () => {
   mainWindow = new BrowserWindow({ show: false });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.once('ready-to-show', () => {
+  mainWindow.once("ready-to-show", () => {
     mainWindow.show();
   });
 
-  mainWindow.on('closed', () => {
+  // getFileFromUserSelection();
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 });
--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,26 @@
-const marked = require('marked');
+const marked = require("marked");
+const { remote } = require("electron");
+const mainProcess = remote.require("./main.js");
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
+const markdownView = document.querySelector("#markdown");
+const htmlView = document.querySelector("#html");
+const newFileButton = document.querySelector("#new-file");
+const openFileButton = document.querySelector("#open-file");
+const saveMarkdownButton = document.querySelector("#save-markdown");
+const revertButton = document.querySelector("#revert");
+const saveHtmlButton = document.querySelector("#save-html");
+
+const renderMarkdownToHtml = markdown => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+markdownView.addEventListener("keyup", event => {
+  console.log(event);
+  console.log(event.target);
+  renderMarkdownToHtml(event.target.value);
+});
+
+openFileButton.addEventListener("click", () => {
+  //   alert("bar horing");
+  mainProcess.getFileFromUserSelection();
+});
